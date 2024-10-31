a27ee37fdf5a43c1990d961b760a06626bbb0a13
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-10-31 18:06:09 +0530

    Update __init__.py
    Signed-off-by: BAD MUNDA <139567638+badmunda011@users.noreply.github.com>

--- FallenRobot/__init__.py
+++ FallenRobot/__init__.py
@@ -39,7 +39,7 @@ if ENV:
     API_ID = int(os.environ.get("API_ID", None))
     API_HASH = os.environ.get("API_HASH", None)
     BOT_ID = int(getenv("BOT_ID", "7894953989"))
-    DB_NAME = int(getenv("DB_NAME", "shizuDB"))
+    DB_NAME = os.environ.get("DB_NAME", None)
     ALLOW_CHATS = os.environ.get("ALLOW_CHATS", True)
     ALLOW_EXCL = os.environ.get("ALLOW_EXCL", False)
     CASH_API_KEY = os.environ.get("CASH_API_KEY", None)
