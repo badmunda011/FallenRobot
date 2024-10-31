236c6d4b7243f4af88c32a8fe7c5a20869c47d51
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-10-31 18:38:26 +0530

    Update time.py
    Signed-off-by: BAD MUNDA <139567638+badmunda011@users.noreply.github.com>

--- time.py
+++ time.py
@@ -1,1 +1,14 @@
+from os import getenv
+from pyrogram import filters
+from dotenv import load_dotenv
+import pytz
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
+load_dotenv()
+
 BAD = "Asia/Kolkata"
+
+
+#time zone
+BAD = pytz.timezone(time.BAD)
+scheduler = AsyncIOScheduler(timezone=BAD)
