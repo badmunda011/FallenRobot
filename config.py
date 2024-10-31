8b8bc13f38bc6921f1bd4ea239ed8162b5fe2218
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-10-31 18:37:51 +0530

    Update config.py
    Signed-off-by: BAD MUNDA <139567638+badmunda011@users.noreply.github.com>

--- config.py
+++ config.py
@@ -1,17 +1,11 @@
 from os import getenv
 from pyrogram import filters
 from dotenv import load_dotenv
-import pytz
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 load_dotenv()
 
 SUKH = "shizuDB"
-BAD = "Asia/Kolkata"
 MONGO_URL = getenv("MONGO_URL", None)
 
 
-#time zone
-BAD = pytz.timezone(time.BAD)
-scheduler = AsyncIOScheduler(timezone=BAD)
 
