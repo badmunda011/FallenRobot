925aa89405553bfe0c58bbb7bf740bd82e3ddb9a
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-10-31 18:43:14 +0530

    Update antispam_db.py
    Signed-off-by: BAD MUNDA <139567638+badmunda011@users.noreply.github.com>

--- FallenRobot/welcome/antispam_db.py
+++ FallenRobot/welcome/antispam_db.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from threading import RLock
 
-from time import TIME_ZONE as TZ
+from FallenRobot.welcome.time import TIME_ZONE as TZ
 from FallenRobot.welcome import MongoDB
 
 INSERTION_LOCK = RLock()
